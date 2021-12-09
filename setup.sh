7491a90e049bb46f5851eb51861b10e361de109f
Miguel Rodrigues dos Santos <miguelrodriguesdossantos@gmail.com>
2021-12-09 18:15:45 +0100

    Wrap it up

--- setup.sh
+++ setup.sh
@@ -1,12 +1,25 @@
-OLDFILESDIR=/var/old_dotfiles/
-echo "Setting new dotfiles; moving previous ones to ${OLDFILESDIR}"
+OLDFILESDIR=dotfiles_backup/
 
+echo "Setting new dotfiles; moving previous ones to $(pwd)/${OLDFILESDIR}"
 mkdir -p ${OLDFILESDIR}
-mv ~/.bash_aliases ~/.vimrc ~/.tmux.conf ${OLDFILESDIR}
 
-ln -s $(pwd)/configs/bash_aliases ~/.bash_aliases
-ln -s $(pwd)/configs/vimrc        ~/.vimrc
-ln -s $(pwd)/configs/tmux.conf    ~/.tmux.conf
-ln -s $(pwd)/starship.conf        ~/.config/starship.toml
+move_to_backup () {
+	mv "$1" ${OLDFILESDIR};
+}
+
+set_symlink    () {
+	ln -s $(pwd)/$1 $2;
+}
+
+deploy_dotfile () {
+	move_to_backup $2;
+	set_symlink $1 $2;
+}
+
+deploy_dotfile    configs/.bash_aliases   ~/.bash_aliases
+deploy_dotfile    configs/.vimrc          ~/.vimrc
+deploy_dotfile    configs/.tmux.conf      ~/.tmux.conf
+deploy_dotfile    configs/starship.toml   ~/.config/starship.toml
+
 tmux source-file ~/.tmux.conf
-
+echo "... done!"
